1378adff72408da14a035caf179161796686af6e
Vane1ssa <kostakokoev1988@gmail.com>
2025-03-16 17:08:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// button script js
+
+$('.btn').on('mouseenter', function(e){
+    let parentOffset = $(this).offset(),
+    posX = e.pageX - parentOffset.left,
+    posY = e.pageY - parentOffset.top;
+    $(this).find('span').css({top: posY, left: posX});
+});
+
+// preloader script
+
+$(window).on("load", function(){
+    $("#preloader").fadeOut(1000)
+});
+
+// open mobile meni script
+
+let openMenu = $('#open-menu');
+let closeMenu = $('#close-menu');
+let nav = $('#nav');
+
+openMenu.click(function(){
+    event.preventDefault();
+    nav.animate({
+        "left": "0rem"
+    }, 500)
+});
+
+closeMenu.click(function(){
+    event.preventDefault();
+    nav.animate({
+        "left": "-30rem"
+    }, 500)
+});
+
+// close menu on click link
+$('.menu li a').on('click', function(){
+    nav.animate({
+        'left': '-30rem'
+    }, 500)
+});
+
+// fixed menu on scroll
+$(window).on('scroll', function(){
+   let headerTop = $('.top-header').height();
+   if ($(this).scrollTop() >= headerTop){
+    $('.header').addClass('fixed-menu');
+   } else {
+    $('.header').removeClass('fixed-menu')
+   }
+});
+
+//magnific popup script
+
+$('.popup-youtube').magnificPopup({
+    type: 'iframe',
+})
